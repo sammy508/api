4f8e3697b157bc112710c8a4f3e919b9cae2a6e8
sammy50 <chapagainsamir9@gmail.com>
2023-10-13 10:23:20 +0545

    merge files

--- lib/getApi_example/home_screen.dart
+++ lib/getApi_example/home_screen.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:projectapione/model/getApi_model/postModel.dart';
+
 import 'package:projectapione/model/postt_model/postModel.dart';
+
+//import 'package:projectapione/model/postt_model/postModel.dart';
 
 class HOmeScreen extends StatefulWidget {
   const HOmeScreen({super.key});
